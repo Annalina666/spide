2aa31c2afd8457fea1c05f5aff8de40baf9772b4
Annalina666 <116889932+Annalina666@users.noreply.github.com>
2022-11-09 15:40:57 +0100

    JS changes

--- павук/index.js
+++ павук/index.js
@@ -2,10 +2,10 @@
 var counter = 0
 
 // Function with Parameter
-function changeText(){
+/* function changeText(){
  let element = document.getElementById("btn");
  if (element.value=="Hide Result")
  	element.value = "Show Result";
  else
  	element.value = "Hide Result";
-}
+}*/
