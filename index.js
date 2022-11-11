4a7d3824d78643d4409b281d28746e1df8cff517
Annalina666 <116889932+Annalina666@users.noreply.github.com>
2022-11-11 12:26:41 +0100

    .

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 // Variables
 var counter = 0
-
-// Function with Parameter
-/* function changeText(){
- let element = document.getElementById("btn");
- if (element.value=="Hide Result")
- 	element.value = "Show Result";
- else
- 	element.value = "Hide Result";
-}*/
