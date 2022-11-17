f5daa8e73ad9e78202787a391fa2ab26ed080f0a
Annalina666 <116889932+Annalina666@users.noreply.github.com>
2022-11-17 11:25:45 +0100

    button

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // Variables
 var counter = 0
+
+document.getElementById("button1").style.backgroundColor ="gray";
+document.getElementById("button2").style.backgroundColor ="gray";
+
+
+document.getElementById("button1").onclick = function(){
+            this.style.backgroundColor ="black";
+            document.getElementById("button2").style.backgroundColor ="gray";
+        };
+
+document.getElementById("button2").onclick = function(){
+            this.style.backgroundColor ="black";
+            document.getElementById("button1").style.backgroundColor ="gray";
+        };
