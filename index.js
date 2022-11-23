2900cd7f16000f0c65d75c04dddd0a76088e8ead
Annalina666 <116889932+Annalina666@users.noreply.github.com>
2022-11-23 14:00:53 +0100

    с

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 // Variables
 var counter = 0
-
-document.getElementById("button1").style.backgroundColor ="gray";
-document.getElementById("button2").style.backgroundColor ="gray";
-
-
-document.getElementById("button1").onclick = function(){
-            this.style.backgroundColor ="black";
-            document.getElementById("button2").style.backgroundColor ="gray";
-        };
-
-document.getElementById("button2").onclick = function(){
-            this.style.backgroundColor ="black";
-            document.getElementById("button1").style.backgroundColor ="gray";
-        };
